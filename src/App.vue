77ac101c1603e7cd86b6a7f39ae8da5b1bceac72
q11c12f <216102388@qq.com>
2023-08-24 21:04:37 +0800

    8.24提交

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,53 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="leftbox">
+      <!-- 3.使用组件 -->
+      <Leftbox msg="Welcome to Your Vue.js App"/>
+    </div>
+    <!-- <div class="rightContent">
+        <router-view></router-view>
+    </div> -->
   </div>
 </template>
 
 <script>
+// 1.引入组件
+import Leftbox from './components/index.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  // 2.挂载组件实例
+  components: {
+    Leftbox
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+}
+#app .leftBar{
+  width:20vw;
+  border-right:5px solid red;
+  margin-right:10px;
+}
+#app .rightContent{
+    /* width:80vw; */
+    flex: 1;
+    height:90vh;
+    /* background: #fdffcb; */
+    /* font-size:40px; */
+}
+/* 屏幕宽度小于992px，则执行如下样式，样式权重要高于其他的样式*/
+@media screen and (max-width:992px){
+  #app .leftBar{
+   display: none;
+  }
 }
 </style>
---
+++ src/components/index.vue
@@ -0,0 +1,26 @@
+<script src="../../src/icon"></script>
+<template>
+  <div class="shouye">
+     <div class="top">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-fenlei"></use>
+      </svg>
+         灵活用工服务平台
+     </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
